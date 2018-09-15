bcd149a480095ddb0be42bb946f585d7b081187f
隼斗 小林 <wahuku.miyatuguti_35@hotmail.co.jp>
2018-09-15 21:34:50 +0900

    送信できる

--- MyastiaAzure/MainPage.xaml.cs
+++ MyastiaAzure/MainPage.xaml.cs
@@ -1,17 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+
+using System.Text;
+using System.Diagnostics;
+using Microsoft.Azure.Devices.Client;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
 
 // 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
 
@@ -25,6 +22,39 @@ namespace MyastiaAzure
         public MainPage()
         {
             this.InitializeComponent();
+
+            Loaded += MainPage_Loaded;
+            Unloaded += MainPage_Unloaded;
         }
+
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            var myDevice = new SensorDevice();
+            await myDevice.Run();
+
+            this.TextBox01.Text = "完了";
+        }
+
+        private void MainPage_Unloaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        //private async Task ReceiveDataFromAzure()
+        //{
+        //    Message receivedMessage;
+        //    string messageData = string.Empty;
+        //    while (true)
+        //    {
+        //        receivedMessage = await deviceClient.ReceiveAsync();
+        //        if (receivedMessage != null)
+        //        {
+        //            messageData = Encoding.UTF8.GetString(receivedMessage.GetBytes());
+        //            Debug.WriteLine(messageData);
+        //            await deviceClient.CompleteAsync(receivedMessage);
+        //        }
+        //    }
+        //}
     }
 }
---
+++ MyastiaAzure/SensorDevice.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace MyastiaAzure
+{
+    public class SensorDevice
+    {
+        private readonly DeviceClient deviceClient;
+        private readonly KeyValuePair<string, string> deviceProperty;
+        private StringBuilder cnString = new StringBuilder();
+        private const string HostName = "MyastiaIot.azure-devices.net";
+        private const string DeviceId = "Rasp01;";
+        private const string SharedAccessKey = "9gbtY1yoTNxNME3RwxighF3u8xqc5j/Tcd49EW7saNc=";
+
+        public SensorDevice()
+        {
+            cnString.Append("HostName=" + HostName + ";");
+            cnString.Append("DeviceId=" + DeviceId + ";");
+            cnString.Append("SharedAccessKey=" + SharedAccessKey);
+            deviceProperty = new KeyValuePair<string, string>("DeviceType", "RasspberryPi");
+
+
+            deviceClient = DeviceClient.CreateFromConnectionString(cnString.ToString(),TransportType.Http1);
+        }
+        ~SensorDevice()
+        {
+            deviceClient.Dispose();
+        }
+        //********************************************************************************
+        //プロパティ
+        //********************************************************************************
+        public string ReceivedMessage { get; set; } = string.Empty;
+
+        //********************************************************************************
+        //メソッド
+        //********************************************************************************
+        public async Task Run()
+        {
+            await SendAzureMessage();
+
+        }
+
+        public async Task SendAzureMessage()
+        {
+            var data = new SensorData();
+            data.DeviceId = DeviceId;
+            data.SensorText = "Use json";
+
+            var dataString = JsonConvert.SerializeObject(data);
+            using (var message = new Message(Encoding.UTF8.GetBytes(dataString)))
+            {
+                message.Properties.Add(deviceProperty);
+                await deviceClient.SendEventAsync(message);
+            }
+        }
+
+        public async Task ReceiveAzureMessage()
+        {
+            while (true)
+            {
+                var message = await deviceClient.ReceiveAsync();
+                if (message == null)
+                {
+                    continue;
+                }
+                var messageText = Encoding.UTF8.GetString(message.GetBytes());
+                await deviceClient.CompleteAsync(message);
+                message.Dispose();
+
+                this.ReceivedMessage = messageText;
+            }
+        }
+
+    }
+
+    class SensorData
+    {
+        public string DeviceId { get; set; } = string.Empty;
+        public string SensorText { get; set; } = string.Empty;
+    }
+}
